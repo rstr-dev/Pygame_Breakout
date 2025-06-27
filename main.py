069e00080fe1be50468d38c44298e39ffbba4f30
rstr-dev <strachana0104@gmail.com>
2025-06-27 17:00:28 -0400

    minor adjustments

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import random
 import pygame
 import time
 
 pygame.init()
+pygame.font.init()
+pygame.display.set_caption("BREAKOUT DEMO")
 
 SCREEN = pygame.display.set_mode( (1680,900))
 # SCREEN = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
@@ -12,7 +13,6 @@ WHITE = (255, 255, 255)
     
 WIDTH, HEIGHT = pygame.display.get_surface().get_size()
 FONT = pygame.font.SysFont('arial', 50)
-pygame.display.set_caption("PONG DEMO")
 SCREEN.fill(BLACK) 
 
 MIDDLE_W = WIDTH / 2
@@ -25,59 +25,97 @@ PADDLE_W = WIDTH / 8
 PADDLE_H = 20
 PADDLE_Y = HEIGHT - (PADDLE_H * 2)
 
+OPTION_WIDTH = 800
+OPTION_HEIGHT = 50
+INNERBOX_OFFSET = 5
+
 PUCK_S = 50 # width and height of the puck
 COMP_SPEED = 1.75
+COLORS = 6
+LIVES = 5
+
+colors = {0: (204, 153, 255), 1: (157, 154, 255), 2: (154, 255, 195), 3: (255, 255, 154), 4: (255, 222, 154), 5: (255, 154, 154), 6: (0, 0, 0)} # 0 is purple 5 is red. 6 is used for a brick that is about to be deleted
 
 class Puck(): # creates a puck that moves across the screen
-    def __init__(self, last_brick):
+    def __init__(self, paddle):
         self.angle = 0.0 # pucks y axis movement
-        self.direction = 1 # puck moves up (-1) or down (1)
-        self.rect = pygame.Rect(MIDDLE_W - (PUCK_S / 2), last_brick.rect.top + last_brick.rect.height + (PUCK_S / 2), PUCK_S, PUCK_S) # creating the rectangle
+        self.direction = 0 # puck moves up (-1) or down (1)
         self.speed = 10.0 # puck movement speed
-        self.lives = 3
-        self.last_brick = last_brick
+        self.lives = LIVES
         
-    def middle(self):
-        return self.rect.left + (self.rect.width / 2)
+        self.rect = pygame.Rect(paddle.rect.width / 2, paddle.rect.top + 2, PUCK_S, PUCK_S) # creating the rectangle
+        
+        self.paddle = paddle
+        self.free = False
+        
+    def let_puck_go(self):
+        self.free = True
+        self.direction = -1
     
     def reset(self):
-        self.rect = pygame.Rect(MIDDLE_W - (PUCK_S / 2), self.last_brick.rect.top + self.last_brick.rect.height + (PUCK_S / 2), PUCK_S, PUCK_S) # creating the rectangle
+        self.rect = pygame.Rect(self.paddle.rect.left + (self.paddle.rect.width / 2 - (self.rect.width / 2)), self.paddle.rect.top - self.rect.height - 2, PUCK_S, PUCK_S) # the puck sits on top of the paddle
+        self.free = False
         self.angle = 0.0 # pucks y axis movement
         self.direction = 1 # puck moves up (-1) or down (1)
         
     def update(self): # redraw the puck
-        self.rect = pygame.Rect(self.rect.left + self.angle, self.rect.top + (self.direction * self.speed), PUCK_S, PUCK_S) # move according to the direction and angle
-        pygame.draw.rect(SCREEN, WHITE, self) # redraw on screen
-       
-colors = {-1: None, 0: (204, 153, 255), 1: (157, 154, 255), 2: (154, 255, 195), 3: (255, 255, 154), 4: (255, 222, 154), 5: (255, 154, 154)}
- 
+        if self.free == False:
+            self.rect = pygame.Rect(self.paddle.rect.left + (self.paddle.rect.width / 2 - (self.rect.width / 2)), self.paddle.rect.top - self.rect.height - 2, PUCK_S, PUCK_S) # the puck sits on top of the paddle
+        else:
+            self.rect = pygame.Rect(self.rect.left + self.angle, self.rect.top + (self.direction * self.speed), PUCK_S, PUCK_S) # move according to the direction and angle
+        pygame.draw.rect(SCREEN, WHITE, self.rect) # redraw on screen       
+        
 class Brick():
     def __init__(self, x, y, health): 
         self.rect = pygame.Rect(x, y, BRICK_W, BRICK_H)
         self.health = health
-        self.color = colors[6 - health]
-        
-    def middle(self):
-        return self.rect.left + (self.rect.width / 2)
-    
-    def subtract_health(self):
-        self.health = self.health - 1
-        print(self.health)
-        self.color = colors[self.health]
+        self.color = colors[COLORS - health]
     
     def update(self):
-        pygame.draw.rect(SCREEN, BLACK, self)
-        inner_rect = pygame.Rect(self.rect.left + 2, self.rect.top - 2, BRICK_W - 2, BRICK_H - 2)
-        pygame.draw.rect(SCREEN, self.color, inner_rect)
+        rect = pygame.Rect(self.rect.left + 2, self.rect.top - 2, BRICK_W - 2, BRICK_H - 2)
+        pygame.draw.rect(SCREEN, self.color, rect)
+        
+    def collision_check(self, puck):
+        if puck.rect.colliderect(self.rect):
+            if self.health > 0:
+                self.health = self.health - 1
+            self.color = colors[COLORS - self.health]
+            return True
+        return False
         
 class Brick_Collection():
-    def __init__(self): 
-        self.collection = [[Brick(r * BRICK_W, c * BRICK_H, 6 - c) for r in range(6)] for c in range(6)] 
+    def __init__(self, rows, cols): 
+        self.rows = rows
+        self.cols = cols
+        self.collection = [[Brick(r * BRICK_W, c * BRICK_H, rows - c) for r in range(rows)] for c in range(cols)] 
     
+    def reset(self):
+        self.collection = [[Brick(r * BRICK_W, c * BRICK_H, self.rows - c) for r in range(self.rows)] for c in range(self.cols)] 
+        
+    def empty(self):
+        for row in self.collection:
+            if len(row) > 0:
+                return False
+        return True     
+        
+    def collision(self, puck):
+        bricks_to_remove = [] # array in case multiple bricks are hit at once
+                        
+        for row in reversed(self.collection): # moving through the array in reverse as it is most likely for the bricks on the bottom to be hit first- this can save some processing time
+            for element in reversed(row):
+                if element.collision_check(puck):
+                        if 1 > element.health:
+                            bricks_to_remove.append([row, element])
+                        puck.direction = 1
+                        get_angle(element.rect, puck)
+
+        for element in bricks_to_remove:
+            element[0].remove(element[1])
+                 
     def update(self):
         for row in self.collection:
             for element in row:
-                element.update()
+                element.update()       
         
 class Paddle(): # creates a paddle
     def __init__(self, x, y): # create paddle
@@ -86,44 +124,45 @@ class Paddle(): # creates a paddle
     def update(self):
         x, y = pygame.mouse.get_pos() # get the users mouse coordinates
         self.rect = pygame.Rect(x - PADDLE_W / 2, self.rect.top, PADDLE_W, PADDLE_H) # the players puck matches their mouses Y axis
-        prevent_off_screen(self)
-        pygame.draw.rect(SCREEN, WHITE, self)
         
-    def middle(self):
-        return self.rect.top + (self.rect.height / 2)
-        
-def prevent_off_screen(paddle):
-    if (paddle.rect.left < 0): # prevent from going off screen top
-            paddle.rect = pygame.Rect(0, PADDLE_Y, PADDLE_W, PADDLE_H)
+        if (self.rect.left < 0): # prevent from going off screen left
+            self.rect = pygame.Rect(0, PADDLE_Y, PADDLE_W, PADDLE_H)
             
-    if (WIDTH < paddle.rect.right): # prevent from going off screen bottom
-        paddle.rect = pygame.Rect(WIDTH - PADDLE_W, PADDLE_Y, PADDLE_W, PADDLE_H)
+        if (WIDTH < self.rect.right): # prevent from going off screen right
+            self.rect = pygame.Rect(WIDTH - PADDLE_W, PADDLE_Y, PADDLE_W, PADDLE_H)
+            
+        pygame.draw.rect(SCREEN, WHITE, self)
+    
+    def collision(self, puck):
+        if self.rect.colliderect(puck.rect):
+            collision_rect = pygame.Rect.clip(self.rect, puck.rect)
+            get_angle(collision_rect, puck)
+            puck.direction = -1
         
-def draw_score(player_wins, comp_wins): # print out the current scores onto the screen
-    FONT = pygame.font.SysFont('arial', 30)
+def draw_information(player_wins, puck): # print out the current scores onto the screen
+    FONT = pygame.font.SysFont("arial", 30)
     player_wins = FONT.render(str(player_wins), False, WHITE)
-    comp_wins = FONT.render(str(comp_wins), False, WHITE)
+    lives = FONT.render(str(puck.lives), False, WHITE)
     
     SCREEN.blit(player_wins, (50,10))
-    SCREEN.blit(comp_wins, (WIDTH - 50, 10))
+    SCREEN.blit(lives, (WIDTH - 50, 10))
 
-def get_angle(element, puck): # get the angle the puck should make depending on its previous collision
-    difference = element.rect.left - puck.rect.left # calculate how close to puck was to the center
+def get_angle(collision_rect, puck): # get the angle the puck should make depending on its previous collision
+    left_edge_closeness = abs(collision_rect.left - puck.rect.left)
+    right_edge_closeness = abs(collision_rect.right - puck.rect.right)
     
-    if element.middle() < puck.rect.left:
-        difference = -difference
-    
-    # if type(element) == Brick:
-    #     difference = -difference
+    if left_edge_closeness < right_edge_closeness: # puck collided with left edge of paddle
+        puck.angle = -((PADDLE_W - left_edge_closeness)/(PADDLE_W / 4))
+    elif right_edge_closeness < left_edge_closeness: # puck collided with right edge of paddle
+        puck.angle = ((PADDLE_W - right_edge_closeness)/(PADDLE_W / 4))
+    else: # puck is center of paddle
+        puck.angle = 0 
         
-    return float(difference/100.0) # the new angle will be relative to how far the puck was from the center. - means the puck was above the center + means the puck was under the center
-
-def check_collision(bricks, puck, player_paddle):
-    if pygame.Rect.colliderect(player_paddle.rect, puck.rect): # puck has hit the paddle
-        puck.direction = -1 # puck turns up
-        puck.angle = get_angle(player_paddle, puck) # new puck X movement
-        puck.rect.bottom = puck.rect.bottom + 5
-    elif puck.rect.top - 5 < 0: # puck has hit the top of the screen
+    if puck.direction == 1:
+        puck.angle = -puck.angle
+        
+def check_collision(bricks, puck, player_paddle):   
+    if puck.rect.top - 5 < 0: # puck has hit the top of the screen
         puck.direction = 1 # puck turns down
     elif puck.rect.left < 0:
         puck.angle = 2
@@ -132,57 +171,42 @@ def check_collision(bricks, puck, player_paddle):
     elif HEIGHT < puck.rect.bottom:
         puck.lives -= 1
         if puck.lives > -1:
-            puck.reset()         
+            puck.reset()  
     else:
-        bricks, puck.direction, puck.angle = check_brick_collision(bricks, puck) # check if the puck collided with a brick
-            
-def check_brick_collision(bricks, puck):
-    remove_row = None
-    remove = None
+        player_paddle.collision(puck) # check if puck collided with paddle
+        bricks.collision(puck) # check if the puck collided with a brick
 
-    for row in reversed(bricks.collection):
-        for element in reversed(row):
-            if puck.rect.colliderect(element.rect):
-                print("hit")
-                if element.health > 0:
-                    element.subtract_health()
-                else:
-                    remove_row = row
-                    remove = element
-                break
-                    
-    if remove != None:
-        remove_row.remove(remove)
-        while puck.rect.colliderect(remove.rect):
-            puck.rect.top += 1
-        
-        return bricks, 1, get_angle(element, puck)
-             
-    return bricks, puck.direction, puck.angle
-        
+def show_options(text):
+    rect = pygame.Rect(MIDDLE_W - (OPTION_WIDTH / 2) + (INNERBOX_OFFSET / 2), MIDDLE_H - (OPTION_HEIGHT / 2) + (INNERBOX_OFFSET / 2), OPTION_WIDTH - INNERBOX_OFFSET, OPTION_HEIGHT - INNERBOX_OFFSET) # the players puck matches their mouses Y axis
+    pygame.draw.rect(SCREEN, BLACK, rect)
+    
+    text = FONT.render(text, False, WHITE)
+    text_rect = text.get_rect(center = (rect.centerx, rect.centery)) # places text in the middle of the square
+    SCREEN.blit(text, text_rect)
+    
 def check_win(puck):
     if puck.rect.left < 10: 
         return 0, 1
     if WIDTH - 10 < puck.rect.left: 
         return 1, 0
-    
     return 0, 0 
-
 
 def game_loop():
     player_wins = 0
     
     exit = False
-    pause = True
     
     frame_cap = 1.0/60
     time_1 = time.perf_counter()
     unprocessed = 0
     
-    bricks = Brick_Collection()
-    last_brick = bricks.collection[len(bricks.collection) - 1][0]
-    puck = Puck(last_brick)
+    title = True
+    pause = False
+    won = False
+    
+    bricks = Brick_Collection(6,6)
     player_paddle = Paddle(WIDTH/2 - PADDLE_W, PADDLE_H) # create a paddle object for the player
+    puck = Puck(player_paddle)
     pygame.font.init()
     
     while not exit:
@@ -197,23 +221,42 @@ def game_loop():
             can_render = True
 
         if can_render:
-            
-            pygame.display.update()
             SCREEN.fill(BLACK)
             
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     exit = True # if the user closes the window close the game
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_SPACE:
-                        pause = not pause
+                    if event.key == pygame.K_RETURN: 
+                        if title:
+                            title = False
+                        elif won:
+                            won = False
+                            puck.lives = LIVES
+                            bricks.reset()
+                        elif pause:
+                            pause = False
+                        else:
+                            pause = True
+                    if puck.free == False and event.key == pygame.K_SPACE:
+                        puck.let_puck_go()
             
-            check_collision(bricks, puck, player_paddle)
-            
-            if not pause:
-                puck.update()
+            if title:
+                show_options("Press space to return- when in game press return to pause")
+            elif pause:
+                show_options("Press return to resume")
+            elif bricks.empty():
+                show_options("You won- press enter to restart")
+                player_wins += 1
+            elif 0 > puck.lives:
+                puck.lives = LIVES
+                bricks.reset()
+            else: 
+                check_collision(bricks, puck, player_paddle)
                 player_paddle.update() # update the players paddle to match their mouse
+                puck.update()
                 bricks.update()
-                pygame.display.update()
+                draw_information(player_wins, puck)
+            pygame.display.update()
         
 game_loop()
